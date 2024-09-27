8a783bcb396574bee1b95c4ec1633d02cc0a0513
MuhammadRivaFachrodhiya <muhammadrivaf@gmail.com>
2024-09-27 15:01:09 +0800

    Pertemuan 4

---
+++ Praktikum-APD/Kelas/Pertemuan 3/Pertemuan 4/pertemuan-4.py
@@ -0,0 +1,82 @@
+
+
+# print("perulangan kw-0")
+
+# batas = 20 
+
+# for i in range(1, batas, 2):
+#     print(f"Perulangan ke-{i}")
+
+# buah_banyak = ["Apel", "Mangga", "Anggur", "Semangka"]
+
+# for buah in buah_banyak:
+#     if buah == "Anggur":
+#         print("Buah anggur ketemu")
+#     break
+
+
+# for baris in range(1,4):
+#     print("Baris ke-", baris)
+#     for kolom in range(1,4):
+#         print(kolom,"kolom", end=" ")
+#     print()
+#     print()
+
+# lagi = "yes"
+# ulang = 0
+# while lagi == "yes":
+#     ulang += 1
+#     print("Mabar")
+#     lagi = input("Mabar lagi ga? ")
+# print("Selesai Nabar!")
+# print(f"Perulangan terjadi sebanyak {ulang} kali")
+
+# for i in range(1,10):
+#     if i == 4:
+#         break
+#     else:
+#         print(f"Perulangan ke-{i}")
+
+# while True:
+#     ulang = input("Mabar lagi? ")
+#     if ulang == "ga":
+#         break
+#     print("Mabar lagi")
+
+# for i in range(1,10):
+#     if i % 2 == 1:
+#         continue
+#     print(f"Perulangan ke-{i}")
+
+
+#matriks
+# for baris in range(5):
+#     for kolom in range(5):
+#         print("*", end=" ")
+#     print()
+
+# #segitigasikusiku
+# for i in range(1,5):
+#     print("* " * i)
+
+# #segitigasamakaki
+# batas = 5
+# for i in range(batas):
+#     print(" " )
+
+# kesempatan = 3
+# username = input("Masukkan Username: ")
+# password = input("Masukkan Password: ")
+# while kesempatan > 0:
+#     username = input("Masukkan Username: ")
+#     password = input("Masukkan Password: ")
+#     if username == "admin" and password == "admin#1234":
+#         print("Anda Berhasil Login")
+#         break
+#     else:
+#         print("Username atau Password anda salah!")
+#         kesempatan -= 1
+#         print(f"Kesempatan anda tersisa {kesempatan} kali")
+# print("Program Utama")
+
+
