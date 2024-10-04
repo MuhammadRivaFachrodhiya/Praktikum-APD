61332790e444e25892d35923f1f9e69bfa5a1664
MuhammadRivaFachrodhiya <muhammadrivaf@gmail.com>
2024-10-04 15:14:10 +0800

    Pertemuan-5

---
+++ Praktikum-APD/Kelas/Pertemuan 3/Pertemuan 5/2409106053_MuhammadRivaFachrodhiya_B1'24_Pertemuan-5.py
@@ -0,0 +1,61 @@
+# data = [1,2,3,4,5, True, [2,3], "Ripa"]
+# print(data[6])
+
+
+# data = ["Apel", "Durian", "Jeruk"]
+# print(data*3)
+# print("="*3)
+
+# del data[2]
+
+# nilai_hapus = data.pop(0)
+
+# data.remove("Apel")
+# print(data)
+# print(nilai_hapus)
+
+# data[0:2] = "Anggur","Semangka"
+# print(data)
+
+# data.extend(["Semangka","Rambutan"])
+# # data.append(True)
+# # print(data)
+# # data.insert(0,"Rambutan")
+# print(data)
+
+# print(len(data))
+
+# data_mahasiswa = [
+#     ["053","Ripa",["Kalkulus","LI"]],
+#     ["053","Muhammad"]
+# ]
+
+# print(data_mahasiswa[0][2][0])
+
+# data_mahasiswa = [
+#     ["Admin","Admin123","admin"],
+#     ["user","user1234","user"]
+# ]
+
+# print(data_mahasiswa[0][2][0])
+
+# buah = ["Apel","Durian","Jeruk"]
+# indeks = 1
+# for data in buah:
+#     print(f"data ke {indeks}")
+#     print(data)
+#     print("="*10)
+#     indeks = indeks + 1
+
+# for index in range(len(buah)):
+#     print(f"data ke {index+1}")
+#     print(buah[index])
+#     print("="*10)
+
+# for index,item in enumerate(buah):
+#     print(f"data ke {index+1}")
+#     print(item)
+
+op = ("a","b","c")
+op1[2] = "z"
+print(op)
