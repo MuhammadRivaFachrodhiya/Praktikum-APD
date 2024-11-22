80e526b95a480e55374598285b5266dc4e003cab
MuhammadRivaFachrodhiya <muhammadrivaf@gmail.com>
2024-11-22 22:58:17 +0800

    Delete Praktikum-APD/Kelas/Pertemuan 3/PA directory

--- Praktikum-APD/Kelas/Pertemuan 3/PA/produk.py
+++
@@ -1,7 +0,0 @@
-produkATK = {
-    1: {"nama": "Kertas Folio", "harga_jual": 1000, "stok": 100, "harga_beli": 500},
-    2: {"nama": "Keras HVS", "harga_jual": 1000, "stok": 500, "harga_beli": 500},
-    3: {"nama": "Buku Tulis", "harga_jual": 3000, "stok": 120, "harga_beli": 2000},
-    4: {"nama": "Pensil", "harga_jual": 2000, "stok": 50, "harga_beli": 1000},
-    5: {"nama": "Pulpen", "harga_jual": 2000, "stok": 50, "harga_beli": 1000},
-}
